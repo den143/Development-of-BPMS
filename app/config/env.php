a93e3c98fc9542dc1427818dcc3fa9094575c16b
Glen <tagrosglen@gmail.com>
2025-12-28 23:29:23 +0800

    Security: Stop tracking env.php and add env.example.php

--- app/config/env.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('SMTP_HOST', 'smtp.gmail.com');
-define('SMTP_PORT', 587);
-define('SMTP_USER', 'secondemailkoini@gmail.com');
-define('SMTP_PASS', 'yxoa yizs zosc xthx'); 
-define('SMTP_FROM_NAME', 'BPMS Website');
-?>
