a5743153560389ce49dbf5106c43928aca613889
Glen <tagrosglen@gmail.com>
2025-12-28 23:28:16 +0800

    add gitignore

--- app/config/env.php
+++ app/config/env.php
@@ -2,7 +2,7 @@
 
 define('SMTP_HOST', 'smtp.gmail.com');
 define('SMTP_PORT', 587);
-define('SMTP_USER', 'secondemailkoini@gmail.com'); // Your email
-define('SMTP_PASS', 'vzfi rfhy udjx upfg'); // Your App Password
-define('SMTP_FROM_NAME', 'BPMS System');
+define('SMTP_USER', 'secondemailkoini@gmail.com');
+define('SMTP_PASS', 'yxoa yizs zosc xthx'); 
+define('SMTP_FROM_NAME', 'BPMS Website');
 ?>
